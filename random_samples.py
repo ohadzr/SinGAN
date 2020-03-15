55fe04b86d366e0922f546d0bf40fd3b4169d5ef
Ohad <ohadzr@gmail.com>
2020-03-15 16:57:54 +0200

    Supporting different Loss functions from arg parse

--- random_samples.py
+++ random_samples.py
@@ -4,12 +4,12 @@ from SinGAN.training import *
 from SinGAN.imresize import imresize
 import SinGAN.functions as functions
 
-
 if __name__ == '__main__':
     parser = get_arguments()
     parser.add_argument('--input_dir', help='input image dir', default='Input/Images')
     parser.add_argument('--input_name', help='input image name', required=True)
-    parser.add_argument('--mode', help='random_samples | random_samples_arbitrary_sizes', default='train', required=True)
+    parser.add_argument('--mode', help='random_samples | random_samples_arbitrary_sizes', default='train',
+                        required=True)
     # for random_samples:
     parser.add_argument('--gen_start_scale', type=int, help='generation start scale', default=0)
     # for random_samples_arbitrary_sizes:
@@ -28,7 +28,8 @@ if __name__ == '__main__':
         if opt.mode == 'random_samples':
             print('random samples for image %s, start scale=%d, already exist' % (opt.input_name, opt.gen_start_scale))
         elif opt.mode == 'random_samples_arbitrary_sizes':
-            print('random samples for image %s at size: scale_h=%f, scale_v=%f, already exist' % (opt.input_name, opt.scale_h, opt.scale_v))
+            print('random samples for image %s at size: scale_h=%f, scale_v=%f, already exist' % (
+            opt.input_name, opt.scale_h, opt.scale_v))
     else:
         try:
             os.makedirs(dir2save)
@@ -38,17 +39,12 @@ if __name__ == '__main__':
             real = functions.read_image(opt)
             functions.adjust_scales2image(real, opt)
             Gs, Zs, reals, NoiseAmp = functions.load_trained_pyramid(opt)
-            in_s = functions.generate_in2coarsest(reals,1,1,opt)
+            in_s = functions.generate_in2coarsest(reals, 1, 1, opt)
             SinGAN_generate(Gs, Zs, reals, NoiseAmp, opt, gen_start_scale=opt.gen_start_scale)
 
         elif opt.mode == 'random_samples_arbitrary_sizes':
             real = functions.read_image(opt)
             functions.adjust_scales2image(real, opt)
             Gs, Zs, reals, NoiseAmp = functions.load_trained_pyramid(opt)
-            in_s = functions.generate_in2coarsest(reals,opt.scale_v,opt.scale_h,opt)
+            in_s = functions.generate_in2coarsest(reals, opt.scale_v, opt.scale_h, opt)
             SinGAN_generate(Gs, Zs, reals, NoiseAmp, opt, in_s, scale_v=opt.scale_v, scale_h=opt.scale_h)
-
-
-
-
-
