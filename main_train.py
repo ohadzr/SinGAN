11f105c57dc9a0e315cde30bf56aba72a4864025
Ohad <ohadzr@gmail.com>
2020-03-15 12:04:54 +0200

    ohad test

--- main_train.py
+++ main_train.py
@@ -5,6 +5,7 @@ import SinGAN.functions as functions
 
 
 if __name__ == '__main__':
+    #ohad
     parser = get_arguments()
     parser.add_argument('--input_dir', help='input image dir', default='Input/Images')
     parser.add_argument('--input_name', help='input image name', required=True)
